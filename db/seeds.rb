fab850cc38909c707310d9ca3dbbf3b2ef695bb3
Becky Osiecki <lobeliaushtar@gmail.com>
2014-08-15 16:54:55 -0400

    Assignment: Show Only Recent Videos
    Build Robust and Production Quality Applications - Lesson 1 #18

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,8 +17,19 @@ User.create!([
 
 Category.create!(name: 'Mystery')
 Category.create!(name: 'Comedy')
+Category.create!(name: 'Reality')
 
 Video.create!(title: 'Monk', description: 'Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.', small_cover: '/tmp/monk.jpg', large_cover: '/tmp/monk_large.jpg', category_id: 1)
 Video.create!(title: 'Family Guy', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.', small_cover: '/tmp/family_guy.jpg', large_cover: '', category_id: 2)
 Video.create!(title: 'Futurama', description: "Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999.", small_cover: '/tmp/futurama.jpg', large_cover: '', category_id: 2)
 Video.create!(title: 'South Park', description: 'Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.', small_cover: '/tmp/south_park.jpg', large_cover: '', category_id: 2)
+
+Video.create!(title: 'Monk2', description: 'Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.', small_cover: '/tmp/monk.jpg', large_cover: '/tmp/monk_large.jpg', category_id: 1)
+Video.create!(title: 'Family Guy2', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.', small_cover: '/tmp/family_guy.jpg', large_cover: '', category_id: 2)
+Video.create!(title: 'Futurama2', description: "Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999.", small_cover: '/tmp/futurama.jpg', large_cover: '', category_id: 2)
+Video.create!(title: 'South Park2', description: 'Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.', small_cover: '/tmp/south_park.jpg', large_cover: '', category_id: 2)
+
+Video.create!(title: 'Monk3', description: 'Adrian Monk is a brilliant San Francisco detective, whose obsessive compulsive disorder just happens to get in the way.', small_cover: '/tmp/monk.jpg', large_cover: '/tmp/monk_large.jpg', category_id: 1)
+Video.create!(title: 'Family Guy3', description: 'In a wacky Rhode Island town, a dysfunctional family strive to cope with everyday life as they are thrown from one crazy scenario to another.', small_cover: '/tmp/family_guy.jpg', large_cover: '', category_id: 2)
+Video.create!(title: 'Futurama3', description: "Fry, a pizza guy is accidentally frozen in 1999 and thawed out New Year's Eve 2999.", small_cover: '/tmp/futurama.jpg', large_cover: '', category_id: 2)
+Video.create!(title: 'South Park3', description: 'Follows the misadventures of four irreverent grade-schoolers in the quiet, dysfunctional town of South Park, Colorado.', small_cover: '/tmp/south_park.jpg', large_cover: '', category_id: 2)
