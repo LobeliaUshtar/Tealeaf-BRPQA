69f2a35dc8ab8f3cd127211edbcb00440b3674bd
Becky Osiecki <lobeliaushtar@gmail.com>
2014-08-17 19:33:47 -0400

    Assignment: Add authentication
    Build Robust and Production Quality Applications - Lesson 1 #22

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,24 @@
 class SessionsController < ApplicationController
   def new
-    
+    redirect_to home_path if current_user
+  end
+
+  def create
+    user = User.where(email: params[:email]).first
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      # flash[:notice] = "Welcome, you've signed in."
+      redirect_to home_path, notice: "Welcome, you've signed in."
+    else
+      flash[:error] = "Invalid email and/or password."
+      redirect_to sign_in_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    # flash[:notice] = "You've signed out."
+    redirect_to root_path, notice: "You've signed out."
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 Myflix::Application.routes.draw do
   root to: 'pages#front'
 
+  get 'home', to: 'videos#index'
+
   resources :videos, only: [:index, :show] do
     collection do
       get :search, to: 'videos#search'
     end
   end
   resources :users, only: [:create]
-  # resources :categories
+  resources :sessions, only: [:create, :destroy]
+  resources :categories
 
    get 'ui(/:action)', controller: 'ui'
    get 'register', to: 'users#new'
    get 'sign_in', to: 'sessions#new'
+   get 'sign_out', to: 'sessions#destroy'
 end
